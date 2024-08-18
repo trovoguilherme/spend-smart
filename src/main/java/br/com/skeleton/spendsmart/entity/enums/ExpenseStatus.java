d55490cdc294d1ccd45b29b4638dae3891ad7fd7
Guilherme Trovo <xguilhermetrovo@gmail.com>
2024-08-17 23:44:51 -0300

    Criando enums de status e tipo gastos do Expense

---
+++ src/main/java/br/com/skeleton/spendsmart/entity/enums/ExpenseStatus.java
@@ -0,0 +1,9 @@
+package br.com.skeleton.spendsmart.entity.enums;
+
+public enum ExpenseStatus {
+
+    PENDING,
+    PAID,
+    CANCELLED
+
+}
---
+++ src/main/java/br/com/skeleton/spendsmart/entity/enums/ExpenseType.java
@@ -0,0 +1,8 @@
+package br.com.skeleton.spendsmart.entity.enums;
+
+public enum ExpenseType {
+
+    UTILITY_BILLS,
+    CREDIT_CARD_BILLS
+
+}
