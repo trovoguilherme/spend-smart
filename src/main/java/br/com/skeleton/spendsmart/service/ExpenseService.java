2e65d2ffdc7126e87bc609ed90d371582a92fa83
Guilherme Trovo <xguilhermetrovo@gmail.com>
2024-08-22 00:21:15 -0300

    Refatorando save e pay

--- src/main/java/br/com/skeleton/spendsmart/service/ExpenseService.java
+++ src/main/java/br/com/skeleton/spendsmart/service/ExpenseService.java
@@ -2,56 +2,51 @@ package br.com.skeleton.spendsmart.service;
 
 import br.com.skeleton.spendsmart.entity.Expense;
 import br.com.skeleton.spendsmart.entity.Installment;
-import br.com.skeleton.spendsmart.entity.enums.ExpenseStatus;
 import br.com.skeleton.spendsmart.exception.BusinessException;
 import br.com.skeleton.spendsmart.repository.ExpenseRepository;
-import br.com.skeleton.spendsmart.repository.InstallmentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Comparator;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ExpenseService {
 
-    private final ExpenseRepository expenseRepository;
-    private final InstallmentRepository installmentRepository;
+    private final ExpenseRepository repository;
+    private final InstallmentService installmentService;
 
     public List<Expense> findAll() {
-        return expenseRepository.findAll();
+        return repository.findAll();
+    }
+
+    public Expense findById(final Long id) {
+        return repository.findById(id).orElseThrow(() -> new BusinessException("Id not found"));
     }
 
     @Transactional
     public Expense save(Expense expense) {
-        if (expense.getInstallment() != null) {
+        Expense expenseSaved = repository.save(expense);
+        expense.getInstallments().forEach(installment -> installment.setExpense(expense));
+        installmentService.saveAll(expense.getInstallments());
 
-            expense.getInstallment().validateAmount(expense.getValue());
-            expense.getInstallment().computePendingValue();
-            expense.getInstallment().computePaidValue();
-
-            Installment installment = installmentRepository.save(expense.getInstallment());
-            expense.setInstallment(installment);
-        }
-
-        expense.setStatus(ExpenseStatus.PENDING);
-
-        return expenseRepository.save(expense);
-    }
-
-    public Expense update(final Long id) {
-        Expense expense = expenseRepository.findById(id).get();
-        expense.setName("alterado");
-        return expenseRepository.save(expense);
+        return expenseSaved;
     }
 
     public Expense pay(Long id) {
-        Expense expense = expenseRepository.findByIdWithInstallment(id)
-                .orElseThrow(() -> new BusinessException("Id not found"));
-        expense.pay();
+        Expense expense = findById(id);
 
-        return expenseRepository.save(expense);
+        if (expense.getInstallments() != null) {
+            List<Installment> installments = installmentService.pay(expense.getInstallments());
+
+            expense.setInstallments(installments);
+
+            return expense;
+        } else {
+            //TODO Salvar no histórico
+            return expense;
+        }
     }
-
 }
