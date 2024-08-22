5c3c95242ed7ec6b338e33ea5ed902ce12878d27
Guilherme Trovo <xguilhermetrovo@gmail.com>
2024-08-22 00:18:12 -0300

    Retirando a query

--- src/main/java/br/com/skeleton/spendsmart/repository/ExpenseRepository.java
+++ src/main/java/br/com/skeleton/spendsmart/repository/ExpenseRepository.java
@@ -2,16 +2,11 @@ package br.com.skeleton.spendsmart.repository;
 
 import br.com.skeleton.spendsmart.entity.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 
-    @Query("SELECT e FROM Expense e LEFT JOIN FETCH e.installment WHERE e.id = :id")
-    Optional<Expense> findByIdWithInstallment(@Param("id") Long id);
+
 
 }
