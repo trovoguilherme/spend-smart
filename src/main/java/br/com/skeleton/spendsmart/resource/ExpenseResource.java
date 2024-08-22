b5613c72b7581d30367ca355686e6bce056680fa
Guilherme Trovo <xguilhermetrovo@gmail.com>
2024-08-22 00:22:45 -0300

    Refatorando resource

--- src/main/java/br/com/skeleton/spendsmart/resource/ExpenseResource.java
+++ src/main/java/br/com/skeleton/spendsmart/resource/ExpenseResource.java
@@ -3,6 +3,7 @@ package br.com.skeleton.spendsmart.resource;
 import br.com.skeleton.spendsmart.entity.Expense;
 import br.com.skeleton.spendsmart.mapper.ExpenseMapper;
 import br.com.skeleton.spendsmart.resource.request.ExpenseRequest;
+import br.com.skeleton.spendsmart.resource.response.ExpenseResponse;
 import br.com.skeleton.spendsmart.service.ExpenseService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -11,11 +12,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -24,26 +25,22 @@ import java.util.List;
 public class ExpenseResource {
 
     private final ExpenseMapper expenseMapper;
-    private final ExpenseService expenseService;
+    private final ExpenseService service;
 
     @GetMapping
-    public ResponseEntity<List<Expense>> findAll() {
-        return ResponseEntity.ok(expenseService.findAll());
+    public ResponseEntity<List<ExpenseResponse>> findAll() {
+        return ResponseEntity.ok(service.findAll().stream().map(expenseMapper::toExpenseResponse).toList());
     }
 
     @PostMapping
-    public ResponseEntity<Expense> save(@RequestBody @Valid ExpenseRequest expenseRequest) {
-        return ResponseEntity.ok(expenseService.save(expenseMapper.toExpense(expenseRequest)));
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Expense> update(@PathVariable final Long id) {
-        return ResponseEntity.ok(expenseService.update(id));
+    public ResponseEntity<ExpenseResponse> save(@RequestBody @Valid ExpenseRequest expenseRequest) {
+        Expense expense = service.save(expenseMapper.toExpense(expenseRequest));
+        return ResponseEntity.created(URI.create("/" + expense.getId())).body(expenseMapper.toExpenseResponse(expense));
     }
 
     @PatchMapping("/{id}/pay")
-    public ResponseEntity<Expense> payExpense(@PathVariable final Long id) {
-        return ResponseEntity.ok(expenseService.pay(id));
+    public ResponseEntity<ExpenseResponse> pay(@PathVariable final Long id) {
+        return ResponseEntity.ok(expenseMapper.toExpenseResponse(service.pay(id)));
     }
 
 }
