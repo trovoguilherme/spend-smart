c8e18a2a8a6d40e5f6261fd02d34e205bd1c12a7
Guilherme Trovo <xguilhermetrovo@gmail.com>
2024-08-22 00:22:32 -0300

    Criando response

--- src/main/java/br/com/skeleton/spendsmart/resource/response/ExpenseResponse.java
+++ src/main/java/br/com/skeleton/spendsmart/resource/response/ExpenseResponse.java
@@ -1,4 +1,20 @@
 package br.com.skeleton.spendsmart.resource.response;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class ExpenseResponse {
+
+    private String name;
+
+    private String value;
+
+    private List<InstallmentResponse> installments;
+
+    private String status;
+
+    private String type;
+
 }
