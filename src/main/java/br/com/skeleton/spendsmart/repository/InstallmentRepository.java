1f32cebf8fcb96eb7577b2fcbd35ec8768980b04
Guilherme Trovo <xguilhermetrovo@gmail.com>
2024-08-22 22:10:12 -0300

    Criando findAll

--- src/main/java/br/com/skeleton/spendsmart/repository/InstallmentRepository.java
+++ src/main/java/br/com/skeleton/spendsmart/repository/InstallmentRepository.java
@@ -4,6 +4,11 @@ import br.com.skeleton.spendsmart.entity.Installment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface InstallmentRepository extends JpaRepository<Installment, Long> {
+
+    List<Installment> findAllByExpense_Id(Long id);
+
 }
