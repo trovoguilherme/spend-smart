c5fb20aefb75a31cd3e73ac41b0f3aa6911d98d0
Guilherme Trovo <xguilhermetrovo@gmail.com>
2024-08-22 22:10:32 -0300

    Criando Request

---
+++ src/main/java/br/com/skeleton/spendsmart/resource/request/UpdateExpenseRequest.java
@@ -0,0 +1,12 @@
+package br.com.skeleton.spendsmart.resource.request;
+
+import lombok.Data;
+
+@Data
+public class UpdateExpenseRequest {
+
+    private String name;
+
+    private String type;
+
+}
