953ab8ea8c5a5cde6f7457b810a34f338e0c19cc
Guilherme Trovo <xguilhermetrovo@gmail.com>
2024-08-22 22:09:03 -0300

    Criando existsByName

--- src/main/java/br/com/skeleton/spendsmart/repository/ExpenseRepository.java
+++ src/main/java/br/com/skeleton/spendsmart/repository/ExpenseRepository.java
@@ -4,9 +4,13 @@ import br.com.skeleton.spendsmart.entity.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 
+    Optional<Expense> findByName(String name);
 
+    boolean existsByName(String name);
 
 }
