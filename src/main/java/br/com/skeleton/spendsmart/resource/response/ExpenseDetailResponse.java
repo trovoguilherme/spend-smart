bb690ab4b8430e4f628285592a3318b9a496d0d8
Guilherme Trovo <xguilhermetrovo@gmail.com>
2024-09-01 23:47:22 -0300

    Criando mapper para ExpenseDetailResponse

---
+++ src/main/java/br/com/skeleton/spendsmart/resource/response/ExpenseDetailResponse.java
@@ -0,0 +1,26 @@
+package br.com.skeleton.spendsmart.resource.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ExpenseDetailResponse {
+
+    private String name;
+
+    private Double value;
+
+    private InstallmentDetailResponse installmentDetailResponse;
+
+    private String type;
+
+}
