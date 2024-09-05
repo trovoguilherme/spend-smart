00b979e67d264c0d5653da224a85080f73599635
Guilherme Trovo <xguilhermetrovo@gmail.com>
2024-09-04 21:07:58 -0300

    Criando consultas custom

--- src/main/java/br/com/skeleton/spendsmart/repository/ExpenseRepository.java
+++ src/main/java/br/com/skeleton/spendsmart/repository/ExpenseRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+public interface ExpenseRepository extends JpaRepository<Expense, Long>, CustomExpenseRepository {
 
     Optional<Expense> findByName(String name);
 
