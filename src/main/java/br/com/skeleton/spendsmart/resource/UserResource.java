ebdf1a57dfc8f5243d81758a959637a6e66ce8c2
Guilherme Trovo <gtrovo@uolinc.com>
2024-10-02 01:36:10 -0300

    Melhorando o resource para salvar Usuário

--- src/main/java/br/com/skeleton/spendsmart/resource/UserResource.java
+++ src/main/java/br/com/skeleton/spendsmart/resource/UserResource.java
@@ -1,12 +1,18 @@
 package br.com.skeleton.spendsmart.resource;
 
+import br.com.skeleton.spendsmart.entity.User;
+import br.com.skeleton.spendsmart.mapper.UserMapper;
+import br.com.skeleton.spendsmart.resource.request.UserRequest;
 import br.com.skeleton.spendsmart.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
@@ -17,13 +23,19 @@ import java.net.URI;
 public class UserResource {
 
     private final UserService userService;
+    private final UserMapper userMapper;
 
     @PostMapping
-    public ResponseEntity<Void> save() {
-        log.info("entrando para salvar");
-        userService.save();
-        log.info("Salvo");
-        return ResponseEntity.created(URI.create("")).build();
+    public ResponseEntity<User> save(@RequestBody @Valid UserRequest userRequest) {
+
+        User user = userService.save(userMapper.toEntity(userRequest));
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(user.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(user);
     }
 
 }
