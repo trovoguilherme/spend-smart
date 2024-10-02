fd63bb8071d11cff9a82048bd0aefd71db0f8d21
Guilherme Trovo <gtrovo@uolinc.com>
2024-10-01 23:47:56 -0300

    Criando entidade de usuário

---
+++ src/main/java/br/com/skeleton/spendsmart/entity/User.java
@@ -0,0 +1,33 @@
+package br.com.skeleton.spendsmart.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "USERS")
+public class User {
+
+    @Id
+    @Column(name = "ID_USER")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+}
---
+++ src/main/java/br/com/skeleton/spendsmart/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package br.com.skeleton.spendsmart.repository;
+
+import br.com.skeleton.spendsmart.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+}
