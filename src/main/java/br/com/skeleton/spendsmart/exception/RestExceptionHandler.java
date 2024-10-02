ca6153dff5f3722343738598f9da4bdd1e92abf1
Guilherme Trovo <gtrovo@uolinc.com>
2024-10-02 20:53:38 -0300

    Retirando import

--- src/main/java/br/com/skeleton/spendsmart/exception/RestExceptionHandler.java
+++ src/main/java/br/com/skeleton/spendsmart/exception/RestExceptionHandler.java
@@ -5,7 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.StringJoiner;
