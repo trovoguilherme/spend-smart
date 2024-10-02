ee299fc306557a4cd7d9d56ca9268e76dbaf2bd1
Guilherme Trovo <gtrovo@uolinc.com>
2024-10-02 01:32:39 -0300

    Criando mapper para transportar objetos

---
+++ src/main/java/br/com/skeleton/spendsmart/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package br.com.skeleton.spendsmart.mapper;
+
+import br.com.skeleton.spendsmart.entity.User;
+import br.com.skeleton.spendsmart.resource.request.UserRequest;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface UserMapper {
+
+    User toEntity(UserRequest source);
+
+}
