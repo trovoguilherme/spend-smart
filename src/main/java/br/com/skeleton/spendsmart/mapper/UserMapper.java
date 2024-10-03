c321413949fea1bfed5bbcc3c2457638dbcb14fe
Guilherme Trovo <gtrovo@uolinc.com>
2024-10-03 18:28:45 -0300

    Criando repository

--- src/main/java/br/com/skeleton/spendsmart/mapper/UserMapper.java
+++ src/main/java/br/com/skeleton/spendsmart/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package br.com.skeleton.spendsmart.mapper;
 
 import br.com.skeleton.spendsmart.entity.User;
+import br.com.skeleton.spendsmart.entity.Wallet;
 import br.com.skeleton.spendsmart.resource.request.UserRequest;
 import br.com.skeleton.spendsmart.resource.response.UserResponse;
 import org.mapstruct.Mapper;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public interface UserMapper {
 
     @Mapping(target = "password", source = "password", qualifiedByName = "mapBCryptPasswordEncoder")
+    @Mapping(target = "wallet",  expression = "java(mapWallet())")
     User toEntity(UserRequest source);
 
     UserResponse toResponse(User source);
@@ -20,6 +22,10 @@ public interface UserMapper {
     @Named("mapBCryptPasswordEncoder")
     default String mapBCryptPasswordEncoder(String source) {
         return new BCryptPasswordEncoder().encode(source);
+    }
+
+    default Wallet mapWallet() {
+        return Wallet.builder().balance(0.0).build();
     }
 
 }
