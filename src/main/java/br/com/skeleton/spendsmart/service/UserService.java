1e58bed2ab242a744e46ee16ac42076042bceb6d
Guilherme Trovo <gtrovo@uolinc.com>
2024-10-02 22:45:59 -0300

    Adicionando a partde de troca de senha para usuário autenticado

--- src/main/java/br/com/skeleton/spendsmart/service/UserService.java
+++ src/main/java/br/com/skeleton/spendsmart/service/UserService.java
@@ -1,9 +1,13 @@
 package br.com.skeleton.spendsmart.service;
 
 import br.com.skeleton.spendsmart.entity.User;
+import br.com.skeleton.spendsmart.exception.PasswordNotMatchesException;
 import br.com.skeleton.spendsmart.exception.UsernameAlreadyExistsException;
 import br.com.skeleton.spendsmart.repository.UserRepository;
+import br.com.skeleton.spendsmart.resource.request.UpdatePasswordRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +15,8 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final AuthenticatedUserService authenticatedUserService;
+    private final PasswordEncoder passwordEncoder;
 
     public User save(User user) {
         if (userRepository.existsByUsername(user.getUsername())) {
@@ -19,5 +25,21 @@ public class UserService {
 
         return userRepository.save(user);
     }
+
+    public User updatePassword(UpdatePasswordRequest updatePasswordRequest) {
+        String username = authenticatedUserService.getAuthenticatedUsername();
+
+        User userFound = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        if (!passwordEncoder.matches(updatePasswordRequest.getOldPassword(), userFound.getPassword())) {
+            throw new PasswordNotMatchesException();
+        }
+
+        userFound.setPassword(passwordEncoder.encode(updatePasswordRequest.getNewPassword()));
+
+        return userRepository.save(userFound);
+    }
+
 
 }
