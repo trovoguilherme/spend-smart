f32cd20d14a6ea325bf6cd1655804a7a302e6ccf
Guilherme Trovo <gtrovo@uolinc.com>
2024-10-03 18:28:25 -0300

    Mapeando o User com Wallet OneToOne

--- src/main/java/br/com/skeleton/spendsmart/entity/User.java
+++ src/main/java/br/com/skeleton/spendsmart/entity/User.java
@@ -1,5 +1,6 @@
 package br.com.skeleton.spendsmart.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -7,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -47,5 +49,8 @@ public class User {
 
     @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
     private List<Expense> expenses;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Wallet wallet;
 
 }
