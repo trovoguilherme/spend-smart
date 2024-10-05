cca9d370a3a106aedd56cd6aa2e6d73fec96dc38
Guilherme Trovo <xguilhermetrovo@gmail.com>
2024-10-04 23:13:29 -0300

    Adcionando update total do salário por banco

--- src/main/java/br/com/skeleton/spendsmart/service/BankAccountService.java
+++ src/main/java/br/com/skeleton/spendsmart/service/BankAccountService.java
@@ -2,6 +2,7 @@ package br.com.skeleton.spendsmart.service;
 
 import br.com.skeleton.spendsmart.entity.BankAccount;
 import br.com.skeleton.spendsmart.entity.Wallet;
+import br.com.skeleton.spendsmart.exception.NotFoundException;
 import br.com.skeleton.spendsmart.repository.BankAccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,15 +14,23 @@ public class BankAccountService {
     private final BankAccountRepository bankAccountRepository;
     private final UserService userService;
 
-    public BankAccount save(BankAccount bankAccount) {
+    public BankAccount deposit(BankAccount bankAccount) {
+        final Wallet wallet = userService.findByUsername().getWallet();
+
+        return bankAccountRepository.findByBankTypeAndWalletId(bankAccount.getBankType(), wallet.getId())
+                .map(existingAccount -> depositBalanceExistingAccount(existingAccount, bankAccount.getBalance()))
+                .orElseGet(() -> createNewBankAccount(bankAccount, wallet));
+    }
+
+    public BankAccount updateBalance(BankAccount bankAccount) {
         final Wallet wallet = userService.findByUsername().getWallet();
 
         return bankAccountRepository.findByBankTypeAndWalletId(bankAccount.getBankType(), wallet.getId())
                 .map(existingAccount -> updateExistingAccount(existingAccount, bankAccount.getBalance()))
-                .orElseGet(() -> createNewBankAccount(bankAccount, wallet));
+                .orElseThrow(() -> new NotFoundException("Bank account not found"));
     }
 
-    private BankAccount updateExistingAccount(BankAccount existingAccount, Double depositAmount) {
+    private BankAccount depositBalanceExistingAccount(BankAccount existingAccount, Double depositAmount) {
         existingAccount.deposit(depositAmount);
         return bankAccountRepository.save(existingAccount);
     }
@@ -29,6 +38,11 @@ public class BankAccountService {
     private BankAccount createNewBankAccount(BankAccount bankAccount, Wallet wallet) {
         bankAccount.setWallet(wallet);
         return bankAccountRepository.save(bankAccount);
+    }
+
+    private BankAccount updateExistingAccount(BankAccount existingAccount, Double newBalance) {
+        existingAccount.setBalance(newBalance);
+        return bankAccountRepository.save(existingAccount);
     }
 
 }
