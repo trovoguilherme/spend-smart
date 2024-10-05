cd613f345939b16da6b082cc68a493478dbac9f9
Guilherme Trovo <xguilhermetrovo@gmail.com>
2024-10-05 00:49:10 -0300

    Criando método de sacar

--- src/main/java/br/com/skeleton/spendsmart/service/BankAccountService.java
+++ src/main/java/br/com/skeleton/spendsmart/service/BankAccountService.java
@@ -2,6 +2,7 @@ package br.com.skeleton.spendsmart.service;
 
 import br.com.skeleton.spendsmart.entity.BankAccount;
 import br.com.skeleton.spendsmart.entity.Wallet;
+import br.com.skeleton.spendsmart.exception.InsufficientBalanceWithdrawException;
 import br.com.skeleton.spendsmart.exception.NotFoundException;
 import br.com.skeleton.spendsmart.repository.BankAccountRepository;
 import br.com.skeleton.spendsmart.repository.WalletRepository;
@@ -30,6 +31,25 @@ public class BankAccountService {
         return bankAccountRepository.findByBankTypeAndWalletId(bankAccount.getBankType(), wallet.getId())
                 .map(existingAccount -> updateExistingAccount(existingAccount, bankAccount.getBalance()))
                 .orElseThrow(() -> new NotFoundException("Bank account not found"));
+    }
+
+    public BankAccount withdraw(BankAccount bankAccount) {
+        final Wallet wallet = userService.findByUsername().getWallet();
+
+        return bankAccountRepository.findByBankTypeAndWalletId(bankAccount.getBankType(), wallet.getId())
+                .map(existingAccount -> withdrawBalanceFromExistingAccount(existingAccount, bankAccount.getBalance()))
+                .orElseThrow(() -> new NotFoundException("Bank account not found"));
+    }
+
+    private BankAccount withdrawBalanceFromExistingAccount(BankAccount existingAccount, Double amountToWithdraw) {
+        if (existingAccount.getBalance() < amountToWithdraw) {
+            throw new InsufficientBalanceWithdrawException("Insufficient balance for withdrawal");
+        }
+
+        Double previousBalance = existingAccount.getBalance();
+        existingAccount.withdraw(amountToWithdraw);
+        updateWallet(existingAccount.getWallet(), previousBalance, existingAccount.getBalance());
+        return bankAccountRepository.save(existingAccount);
     }
 
     private BankAccount depositBalanceExistingAccount(BankAccount existingAccount, Double depositAmount) {
