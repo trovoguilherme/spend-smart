c51e897376bc01ab7a06a5e835559a0cb2a709c2
Guilherme Trovo <xguilhermetrovo@gmail.com>
2024-10-05 00:48:19 -0300

    Criando método para pegar a wallet

--- src/main/java/br/com/skeleton/spendsmart/resource/UserResource.java
+++ src/main/java/br/com/skeleton/spendsmart/resource/UserResource.java
@@ -4,12 +4,14 @@ import br.com.skeleton.spendsmart.entity.User;
 import br.com.skeleton.spendsmart.mapper.UserMapper;
 import br.com.skeleton.spendsmart.resource.request.UpdatePasswordRequest;
 import br.com.skeleton.spendsmart.resource.request.UserRequest;
+import br.com.skeleton.spendsmart.resource.request.UserWalletResponse;
 import br.com.skeleton.spendsmart.resource.response.UserResponse;
 import br.com.skeleton.spendsmart.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -45,6 +47,11 @@ public class UserResource {
     public ResponseEntity<UserResponse> updatePassword(@RequestBody @Valid UpdatePasswordRequest updatePasswordRequest) {
         User user = userService.updatePassword(updatePasswordRequest);
         return ResponseEntity.ok(userMapper.toResponse(user));
+    }
+
+    @GetMapping("/wallet")
+    public ResponseEntity<UserWalletResponse> getUserWallet() {
+        return ResponseEntity.ok(userMapper.toResponse(userService.getUserWallet()));
     }
 
 }
