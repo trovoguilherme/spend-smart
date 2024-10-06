bd8366f3b3e4842ab02900288637b23001f44a63
Guilherme Trovo <xguilhermetrovo@gmail.com>
2024-10-06 00:04:52 -0300

    Criando dtio para criar investimento

---
+++ src/main/java/br/com/skeleton/spendsmart/resource/request/InvestmentRequest.java
@@ -0,0 +1,38 @@
+package br.com.skeleton.spendsmart.resource.request;
+
+import br.com.skeleton.spendsmart.entity.enums.Bank;
+import br.com.skeleton.spendsmart.entity.enums.InvestmentType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class InvestmentRequest {
+
+    @Positive
+    private Double balance;
+
+    @NotNull
+    private InvestmentType investmentType;
+
+    @NotNull
+    private Bank bankType;
+
+    @Positive
+    private Double yield;
+
+    @NotNull
+    private LocalDate dueDate;
+
+    @NotNull
+    private LocalDate redemptionDate;
+
+}
