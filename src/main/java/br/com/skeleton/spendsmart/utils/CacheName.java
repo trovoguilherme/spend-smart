2660d2f339cc6e5c4948efa6e1ae772d5da24812
Guilherme Trovo <xguilhermetrovo@gmail.com>
2024-11-26 19:52:42 -0300

    Adicionando constante dos nomes do cache

---
+++ src/main/java/br/com/skeleton/spendsmart/utils/CacheName.java
@@ -0,0 +1,10 @@
+package br.com.skeleton.spendsmart.utils;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public final class CacheName {
+
+    public static final String EXPENSES = "expenses";
+
+}
